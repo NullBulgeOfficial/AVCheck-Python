42cf49ecdeb081a495531b8cd3324c8ab02211bc
NullBulgeGroup <NullBulgeOfficial@proton.me>
2024-07-10 22:20:07 -0700

    no message

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="avcheck",
     version="0.1.0",
     description="Python library to interact with AvCheck API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Your Name",
     author_email="your.email@example.com",
     packages=find_packages(),
